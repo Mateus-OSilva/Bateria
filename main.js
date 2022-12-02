0dd6294a39622747be3d141922ade9e81a3dd036
Mateus-OSilva <mateusods20@gmail.com>
2022-12-01 22:05:24 -0300

    Otimizando o código pro laço FOR

--- main.js
+++ main.js
@@ -6,22 +6,15 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-    const tecla = listaDeTeclas[contador]
+    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-
-
-    //template string
-    const idAudio = `#som_${instrumento}`;
-
-    console.log(idAudio)
     tecla.onclick = function () {
-        tocaSom(idAudio);
+        tocaSom(idAudio)
     }
-
-    contador += 1
 
     console.log(contador)
 }
